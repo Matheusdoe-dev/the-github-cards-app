afe0969e2cb45e4c4ec939f60dc16ada67259bfc
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-04-20 07:29:57 -0300

    :construction: feat: Changed state structure on Profiles class component

--- src/pages/Profiles/index.js
+++ src/pages/Profiles/index.js
@@ -3,16 +3,27 @@ import React from 'react';
 import Container from './../../styles/containers/container';
 import Header from './../../components/Header/index'
 // componentes
-import CardList from './../../components/CardList/index';
+import CardList, { testData } from './../../components/CardList/index';
 
 
 class Profiles extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     profiles: testData,
+  //   };
+  // }
+
+  state = {
+    profiles: testData,
+  };
+  
   render(){
     return(
       <Container>
           <Header />
 
-          <CardList />
+          <CardList profiles={this.state.profiles} />
       </Container>
     );
   }
